fdde5e695deea6e532397fc7edd31463199f1c9e
unknown <emrah.pekesen@emrahpekesen-nb.ISTDOM.com>
2016-03-19 15:43:53 +0200

    base App

---
+++ src/main/java/com/aep/spring/FihristApplication.java
@@ -0,0 +1,13 @@
+package com.aep.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FihristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FihristApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/aep/spring/FihristApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aep.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FihristApplication.class)
+public class FihristApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
